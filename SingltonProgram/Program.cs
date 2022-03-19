b82f5068a6b5223df820f8396fbe723f636921d6
TaronHarutyunyan <harutyunyantaron99@mail.ru>
2022-03-19 14:45:10 +0400

    Add project files.

---
+++ SingltonProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace SingltonProgram
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            Mutex mutex = new(false, "Name");
+            if (!mutex.WaitOne(0))
+            {
+                return;
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            
+        }
+    }
+}
